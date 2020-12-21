22e50c2d19e24d9ed1dcec00354f3cf4c7cf0728
Chris <7441139+cbigit@users.noreply.github.com>
2020-12-21 12:36:22 -0700

    Update prerun.sh

--- AI-and-Analytics/prerun.sh
+++ AI-and-Analytics/prerun.sh
@@ -1,8 +1,8 @@
 echo 'source /opt/intel/oneapi/setavrs.sh' >> ~/.bashrc
 echo 'source activate tensorflow' >> ~/.bashrc
 
-git clone https://github.com/oneapi-src/oneAPI-samples.git
-ls
+#git clone https://github.com/oneapi-src/oneAPI-samples.git
+#ls
 
 #source activate tensorflow
 #ls -l Getting-Started-Samples/IntelTensorFlow_GettingStarted/TensorFlow_HelloWorld.py
